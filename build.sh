499557411333702c97f7da3b4dcceb97d56eef1f
Harsh Shandilya <msfjarvis@gmail.com>
2017-03-30 14:07:01 +0530

    build.sh : Back to lineageos defconfig
    Change-Id: I1ae016ce82e909702051a1c302e980bb2b8310fa

--- build.sh
+++ build.sh
@@ -12,7 +12,7 @@ clear
 
 # Resources
 THREAD="-j$(grep -c ^processor /proc/cpuinfo)"
-DEFCONFIG="XOS_jalebi_defconfig"
+DEFCONFIG="lineageos_jalebi_defconfig"
 KERNEL="zImage"
 
 #halogen Kernel Details
